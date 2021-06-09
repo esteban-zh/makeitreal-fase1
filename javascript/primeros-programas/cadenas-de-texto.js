cbf864fe1ff52027aac5268affb1bf7c3a308f4e
esteban-zh <eszahe302@gmail.com>
2021-06-09 11:19:29 -0500

    modulo hasta reto variables de javascript

---
+++ javascript/primeros-programas/cadenas-de-texto.js
@@ -0,0 +1,19 @@
+console.log("mi primer programa de javasccript");
+console.log('mi primer programa de javasccript');
+console.log("mi "+ "programa "+ "de "+ "java" + "script" );
+
+console.log(1 === 1);
+console.log(1 === "1");
+console.log(3 < 2);
+console.log(2 > 0);
+console.log(7 >= 7);
+console.log(6 <= 5);     // false
+console.log(3 !== 3);    // false
+console.log("hola" !== "hola");  // false
+console.log(4 !== "4");          // true
+console.log(false === true);     // false
+console.log(true);      // true
+console.log(false);     // false
+
+
+
---
+++ javascript/variables.js
@@ -0,0 +1,12 @@
+var name = "esteban ";
+console.log("Hola " + name);
+
+var side = 5;
+console.log( "el perimetro del cuadrado de lado " + side + " es " + (side*4));
+console.log( "el area del cuadrado de lado " + side + " es " + (side*side));
+
+var name = "Nayib";
+var age = 34;
+var country = "colombia";
+
+console.log(name + " tiene " + age + " años " + " y vive en " + country);
