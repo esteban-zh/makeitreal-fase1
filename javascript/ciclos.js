d100f8db357ad3ab466a5c45fe5c5ace4417fc3f
esteban-zh <eszahe302@gmail.com>
2021-06-16 10:54:00 -0500

    reto de ciclos

---
+++ javascript/ciclos.js
@@ -0,0 +1,25 @@
+// for (let  i= 1;  i<= 50; i++) {
+//     console.log( i + " estoy aprendiendo");  
+// }
+
+// var i = 1;
+
+// while (i <= 50) {
+//     console.log(i + " estoy aprendiendo");
+//     i++;
+// }
+
+// var i = 1;
+// while (i <= 5) {
+//   console.log(i + " Hola desde el cuerpo del ciclo");
+//   i++;
+// }
+// var i = 0;
+// while ( i < 2 ) {
+//   console.log( (i+1) + " Hola desde el cuerpo del ciclo");
+//   i++;
+// }
+
+for (var i = 0; i < 5; i ++) {
+  console.log("Hola mundo");
+}
